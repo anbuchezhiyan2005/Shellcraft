a260646d6fa5ec1da2ec05b29f71a41fde7b2ed4
Anbuchezhiyan A M <anbuchezhiyanasaithambi@gmail.com>
2026-05-14 15:39:24 +0530

    Refactor: Move commands dictionary to the end of the constant module for better readability

--- app/constant.py
+++ app/constant.py
@@ -3,14 +3,6 @@ import os
 
 path_dirs = os.environ.get("PATH", "").split(os.pathsep)
 
-commands = {
-    "exit": lambda _: sys.exit(0),
-    "echo": lambda args: sys.stdout.write(f"{" ".join(args[1:])} \n"),
-    "type": lambda args: sys.stdout.write(f"{command} is a shell builtin\n") 
-                            if (command := args[1]) in commands 
-                            else helper(command)
-}
-
 def helper(command: str):
     for dir in path_dirs:
         fullPath = os.path.join(dir, command)
@@ -23,3 +15,11 @@ def helper(command: str):
             sys.stderr.write(f"Error: {e} \n")
     
     sys.stdout.write(f"{command} not found\n")
+
+commands = {
+    "exit": lambda _: sys.exit(0),
+    "echo": lambda args: sys.stdout.write(f"{" ".join(args[1:])} \n"),
+    "type": lambda args: sys.stdout.write(f"{command} is a shell builtin\n") 
+                            if (command := args[1]) in commands 
+                            else helper(command)
+}

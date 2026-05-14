0a31eff7a9b7d42dd92bc8651a4126e2bd467af3
Anbuchezhiyan A M <anbuchezhiyanasaithambi@gmail.com>
2026-05-14 18:44:47 +0530

    Refactor: Replace local helper function with utils.helper for better modularity

--- app/constant.py
+++ app/constant.py
@@ -1,22 +1,9 @@
 import sys
-import os
-import shutil
-path_dirs = os.environ.get("PATH", "").split(os.pathsep)
-
-def helper(command: str):
-    fullPath = shutil.which(command)
-    print(fullPath)
-    if fullPath:
-        sys.stdout.write(f"{command} is {fullPath} \n")    
-    else:
-        sys.stdout.write(f"{command} not found\n")
-    
-    return
-
+from app import utils
 commands = {
     "exit": lambda _: sys.exit(0),
     "echo": lambda args: sys.stdout.write(f"{" ".join(args[1:])} \n"),
     "type": lambda args: sys.stdout.write(f"{arg} is a shell builtin\n") 
                             if (arg := args[1]) in commands 
-                            else helper(arg)
+                            else utils.helper(arg)
 }

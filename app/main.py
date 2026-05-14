4b9f0b775b2ac21e545f472301e6a86e37f21032
Anbuchezhiyan A M <anbuchezhiyanasaithambi@gmail.com>
2026-05-14 14:28:08 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,20 +1,19 @@
 import sys
 commands = {
-    "exit": lambda _: sys.exit(0),
-    "echo": lambda input: sys.stdout.write(input + "\n"),
-    "type": lambda input: sys.stdout.write(f"{args} is a shell builtin\n") if (args := input[1]) in commands else sys.stdout.write(f"{args} not found\n")
+    "exit": lambda input: sys.exit(0),
+    "echo": lambda input: sys.stdout.write(f"{" ".join(input[1:])} \n"),
+    "type": lambda input: sys.stdout.write(f"{args} is a shell builtin\n") 
+                            if (args := input[1]) in commands 
+                            else sys.stdout.write(f"{args} command not found\n")
 }
+
 def main():
     while True:
         sys.stdout.write("$ ")
         userInput = sys.stdin.readline().strip()
         input = userInput.split()
         command = input[0] if input else ""
-        if command == "exit":
-            commands[command]
-        elif command == "echo":
-            commands[command](" ".join(input[1:]))
-        elif command == "type":
+        if command in commands:
             commands[command](input)
         else:
             pass

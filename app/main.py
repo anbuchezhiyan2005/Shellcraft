0e62e81c330ffba583963c207533c129e7269cb2
Anbuchezhiyan A M <anbuchezhiyanasaithambi@gmail.com>
2026-05-14 15:11:45 +0530

    Refactor: Enhance command handling with a helper function for path resolution in type builtin

--- app/main.py
+++ app/main.py
@@ -1,11 +1,33 @@
 import sys
+import os
+
+path_dirs = os.environ.get("PATH", "").split(os.pathsep)
+
 commands = {
     "exit": lambda _: sys.exit(0),
     "echo": lambda input: sys.stdout.write(f"{" ".join(input[1:])} \n"),
     "type": lambda input: sys.stdout.write(f"{args} is a shell builtin\n") 
                             if (args := input[1]) in commands 
-                            else sys.stdout.write(f"{args} not found\n")
+                            else helper(args)
 }
+
+def helper(command: str):
+    for dir in path_dirs:
+        fullPath = os.path.join(dir, command)
+        try:
+            if os.path.isFile(fullPath) and os.access(fullPath, os.X_OK):
+                sys.stdout.write(f"{command} is {fullPath} \n")
+                return
+            else: 
+                continue
+
+        except Exception as e:
+            sys.stderr.write(f"Error: {e} \n")
+        finally:
+            pass
+    
+    sys.stdout.write("f{command} not found\n")
+
 
 def main():
     while True:

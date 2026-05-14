bed07cc2988d80a3e205a38612d2b0ab48562ee2
Anbuchezhiyan A M <anbuchezhiyanasaithambi@gmail.com>
2026-05-14 14:32:44 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ commands = {
     "echo": lambda input: sys.stdout.write(f"{" ".join(input[1:])} \n"),
     "type": lambda input: sys.stdout.write(f"{args} is a shell builtin\n") 
                             if (args := input[1]) in commands 
-                            else sys.stdout.write(f"{args} command not found\n")
+                            else sys.stdout.write(f"{args} not found\n")
 }
 
 def main():
@@ -15,8 +15,8 @@ def main():
         command = input[0] if input else ""
         if command in commands:
             commands[command](input)
-        else:
-            pass
+        # else:
+        #     sys.stdout.write(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":

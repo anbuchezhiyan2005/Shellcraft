981287a18a6c2fdd7a7498e7515351b92d5643ca
Anbuchezhiyan A M <anbuchezhiyanasaithambi@gmail.com>
2026-05-14 14:12:53 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import sys
 commands = {
     "exit": lambda _: sys.exit(0),
     "echo": lambda input: sys.stdout.write(input[1:] + "\n"),
-    "type": lambda input: sys.stdout.write(f"{args} is a shell builtin\n") if (args := input[0]) in commands else sys.stdout.write(f"{args} not found\n")
+    "type": lambda input: sys.stdout.write(f"{args} is a shell builtin\n") if (args := input[1]) in commands else sys.stdout.write(f"{args} not found\n")
 }
 def main():
     while True:

b19b2653e4322cd9e553fac90bffc116880a75f5
Anbuchezhiyan A M <anbuchezhiyanasaithambi@gmail.com>
2026-05-14 14:12:16 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,21 +1,21 @@
 import sys
 commands = {
-    "exit": lambda input: sys.exit(0),
-    "echo": lambda input: sys.stdout.write(input + "\n"),
-    "type": lambda input: sys.stdout.write(f"{args} is a shell builtin\n") if (args := input) in commands else sys.stdout.write(f"{args} not found\n")
+    "exit": lambda _: sys.exit(0),
+    "echo": lambda input: sys.stdout.write(input[1:] + "\n"),
+    "type": lambda input: sys.stdout.write(f"{args} is a shell builtin\n") if (args := input[0]) in commands else sys.stdout.write(f"{args} not found\n")
 }
 def main():
     while True:
         sys.stdout.write("$ ")
-        input = sys.stdin.readline().strip()
-        command = input.split()[0] if input else ""
-        userInput = " ".join(input[5:])
+        userInput = sys.stdin.readline().strip()
+        input = userInput.split()
+        command = input[0] if input else ""
         if command == "exit":
             commands[command]
         elif command == "echo":
-            commands[command](userInput)
+            commands[command](input)
         elif command == "type":
-            commands[command](userInput)
+            commands[command](input)
         pass
 
 

13e3249e36208a9c4e5279bec2c03f4c97af9fd4
Anbuchezhiyan A M <anbuchezhiyanasaithambi@gmail.com>
2026-05-14 14:08:01 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,22 +1,21 @@
 import sys
-builtin = ["exit", "echo", "type"]
-
+commands = {
+    "exit": lambda input: sys.exit(0),
+    "echo": lambda input: sys.stdout.write(input + "\n"),
+    "type": lambda input: sys.stdout.write(f"{args} is a shell builtin\n") if (args := input) in commands else sys.stdout.write(f"{args} not found\n")
+}
 def main():
     while True:
         sys.stdout.write("$ ")
-        command = sys.stdin.readline().strip()
-        args = command.split()
-        if args[0] == "exit":
-            break
-        elif args[0] == "echo":
-            sys.stdout.write(" ".join(args[1:]) + "\n")
-        elif args[0] == "type":
-            if args[1] in builtin:
-                sys.stdout.write(f"{args[1]} is a shell builtin\n")
-            else:
-                sys.stdout.write(f"{args[1]}: not found\n")
-        else:
-            sys.stdout.write(f"{command}: not found\n")
+        input = sys.stdin.readline().strip()
+        command = input.split(" ")[0] if input else ""
+        userInput = " ".join(input[5:])
+        if command == "exit":
+            commands[command]
+        elif command == "echo":
+            commands[command](userInput)
+        elif command == "type":
+            commands[command](userInput)
         pass
 
 

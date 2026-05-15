8edd872e60c0cd14c9b6a989e5d57d4f9e008ff0
Anbuchezhiyan A M <anbuchezhiyanasaithambi@gmail.com>
2026-05-15 13:50:09 +0530

    Feat: Add 'pwd' command to output current working directory

--- app/constant.py
+++ app/constant.py
@@ -1,9 +1,13 @@
 import sys
+import os
 from app import utils
 commands = {
     "exit": lambda _: sys.exit(0),
     "echo": lambda args: sys.stdout.write(f"{" ".join(args[1:])}\n"),
     "type": lambda args: sys.stdout.write(f"{arg} is a shell builtin\n") 
                             if (arg := args[1]) in commands 
-                            else utils.helper(arg)
+                            else utils.helper(arg),
+    "pwd": lambda _: sys.stdout.write(f"{os.getcwd()}")
 }
+
+

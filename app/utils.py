88e166bc7efd32f66e6da2b4dbe392e24e5af145
Anbuchezhiyan A M <anbuchezhiyanasaithambi@gmail.com>
2026-05-17 11:33:20 +0530

    codecrafters submit [skip ci]

--- app/utils.py
+++ app/utils.py
@@ -46,13 +46,14 @@ def execute_redirection(redirect: str, idx: int, parts: list):
         # if result.returncode != 0:
         #     sys.stderr.write(result.stderr)
         
-        if result.returncode == 0:
+        if redirect != "2>":
             with open(output_file_path, mode = "w", encoding = "utf-8") as file:
                 file.write(result.stdout)
                 sys.stdout.write(f"{result.stdout}")
         else:
             with open(output_file_path, mode = "w", encoding = "utf-8") as file:
                 file.write(result.stderr)
+                sys.stdout.write(f"{result.stdout}")
     
     except Exception as e:
         sys.stderr.write(f"Error: {e}")

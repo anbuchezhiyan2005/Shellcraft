a438bdaedd2d980171a566cde675af2f807fb2db
Anbuchezhiyan A M <anbuchezhiyanasaithambi@gmail.com>
2026-05-17 13:02:35 +0530

    codecrafters submit [skip ci]

--- app/utils.py
+++ app/utils.py
@@ -6,17 +6,19 @@ from collections import namedtuple
 
 RedirectionResult = namedtuple("RedirectionResult", ["redirect", "idx"])
 
+
 def helper(command: str):
     fullPath = shutil.which(command)
     if fullPath:
-        sys.stdout.write(f"{command} is {fullPath}\n")    
+        sys.stdout.write(f"{command} is {fullPath}\n")
     else:
         sys.stdout.write(f"{command} not found\n")
+
 
 def check_redirection(parts: list):
     if not parts:
         return RedirectionResult("", -1)
-    
+
     redirect = ""
     idx = -1
     if ">" in parts:
@@ -37,44 +39,45 @@ def check_redirection(parts: list):
     elif "2>>" in parts:
         idx = parts.index("2>>")
         redirect = "2>>"
-    
+
     return RedirectionResult(redirect, idx)
 
+
 def execute_redirection(redirect: str, idx: int, parts: list):
-    LHS_command = parts[: idx]
+    LHS_command = parts[:idx]
     if idx + 1 >= len(parts):
         return
-    
+
     output_file_path = parts[idx + 1]
 
     try:
         create_directory_for_file(output_file_path)
 
-        result = subprocess.run(LHS_command, capture_output = True, text = True)
+        result = subprocess.run(LHS_command, capture_output=True, text=True)
 
-        if redirect in (">", "1>", "2>"):  
-            with open(output_file_path, mode = "w", encoding = "utf-8") as file:
+        if redirect in (">", "1>", "2>"):
+            with open(output_file_path, mode="w", encoding="utf-8") as file:
                 file.write(result.stderr if redirect == "2>" else result.stdout)
-            
+
             if redirect in ("1>", ">") and result.stderr:
                 sys.stderr.write(result.stderr)
-        
+
             if redirect == "2>" and result.stdout:
                 sys.stdout.write(result.stdout)
 
         if redirect in (">>", "1>>"):
-            with open(output_file_path, mode = "a", encoding = "utf-8") as file:
+            with open(output_file_path, mode="a", encoding="utf-8") as file:
                 file.write(result.stderr if redirect == "2>>" else result.stdout)
-            
+
             if redirect in (">>", "1>>") and result.stderr:
                 sys.stderr.write(result.stderr)
-            
+
             if redirect == "2>>" and result.stdout:
                 sys.stdout.write(result.stdout)
-    
 
     except Exception as e:
         sys.stderr.write(f"Error: {e}")
+
 
 def execute(parts: list):
     command = parts[0]
@@ -85,9 +88,10 @@ def execute(parts: list):
             execute_redirection(redirect, idx, parts)
         else:
             subprocess.run(parts)
-            
+
     else:
         sys.stderr.write(f"{command}: command not found\n")
+
 
 def check_directory(parts: list):
     path = " ".join(parts[1:])
@@ -95,7 +99,7 @@ def check_directory(parts: list):
 
     if path == "~":
         try:
-            home_path = os.path.expanduser('~')
+            home_path = os.path.expanduser("~")
             os.chdir(home_path)
         except Exception as e:
             sys.stderr.write(f"Error: {e}\n")
@@ -108,21 +112,20 @@ def check_directory(parts: list):
             sys.stderr.write(f"Error: {e}\n")
             os.chdir(curr_path)
     else:
-        sys.stdout.write(f"cd: {path}: No such file or directory\n")    
+        sys.stdout.write(f"cd: {path}: No such file or directory\n")
+
 
 def create_directory_for_file(file_path: str):
     directory = os.path.dirname(os.path.abspath(file_path))
     if directory:
         try:
-            os.makedirs(directory, exist_ok = True)
+            os.makedirs(directory, exist_ok=True)
         except Exception as e:
             sys.stderr.write(f"Error creating directory {directory}: {e}\n")
+
 
 def check_builtin(command: str, command_dict: dict):
     if command in command_dict:
         sys.stdout.write(f"{command} is a shell builtin\n")
     else:
         helper(command)
-
-
-
